01771fb32ed93e7f4c0b098187a0b0fb09104565
anoop.soman <anoop.codeword@gmail.com>
2022-04-04 12:30:45 +0530

    data reading changes from local storage

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -2,17 +2,21 @@ import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges
 
 @Component({
   selector: 'app-sample',
-  template: `
-  <div style="border: blue solid 1px">
-    <p>Data from Parent: {{dataFromParent}}</p>
+  template: `<div style="border: blue solid 1px">
+    <p>Micro App</p>
+    <p>Data from Parent via EventEmitter: {{dataFromParent}}</p>
+    <p>Data from Parent from LocalStorage:</p>
+    <p>Workstation Id: {{workstationid}}</p>
+    <p>Office Id: {{officeid}}</p>
     <input [(ngModel)]="input" [value]="input" (keydown.enter)="send()">
-  </div>
-  `,
+  </div>`,
   styles: []
 })
 export class SampleComponent implements OnChanges, OnInit {
   @Input() dataFromParent: string;
   @Output() emitDataToParent = new EventEmitter<string>();
+  workstationid: string;
+  officeid: string;
   input: string;
   ifLoaded = false;
   constructor() { }
@@ -26,6 +30,8 @@ export class SampleComponent implements OnChanges, OnInit {
     if (this.ifLoaded) {
       // this code is only going to be run once
       console.log(this.dataFromParent);
+      this.workstationid = localStorage.getItem('WorkStationId');
+      this.officeid = localStorage.getItem('OfficeId');
     }
   }
 
